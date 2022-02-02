ceca3389985ce0c2e166294eb5f7bbf9da408df1
brknkl <berkanankal68@gmail.com>
2022-02-02 11:38:50 +0300

    read operation completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const TodoList = () => {
+  const { todos } = useSelector((state) => state.todos);
+
   return (
     <ul className="todo-list">
-      <li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn React</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Have a life!</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <li key={todo.id} className={todo.completed ? "completed" : ""}>
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            <label>{todo.text}</label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
