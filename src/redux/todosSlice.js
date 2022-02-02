ab0c5719ee7d7e6d99b9756b39226e0675fde7d0
brknkl <berkanankal68@gmail.com>
2022-02-02 11:34:46 +0300

    redux toolkit implemented

---
+++ src/redux/todosSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    todos: [
+      { id: "1", text: "Learn JavaScript", completed: true },
+      { id: "2", text: "Learn React", completed: false },
+      { id: "3", text: "Have a life!", completed: false },
+    ],
+  },
+  reducers: {},
+});
+
+export default todosSlice.reducer;
